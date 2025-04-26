ba0332056737653847b5a824da17847404a7475b
Raphael Nussbaumer <rap.n@icloud.com>
2025-04-26 16:39:12 +0200

    bubblewrap init

---
+++ twa/app/src/main/java/com/ciphernotes/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.ciphernotes.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
