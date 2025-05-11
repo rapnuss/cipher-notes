f8237abbd463d697ea9085a23db49b6533bb1641
Raphael Nussbaumer <rap.n@icloud.com>
2025-05-11 13:59:53 +0200

    action icon with text forward ref

--- frontend/src/components/ActionIconWithText.tsx
+++ frontend/src/components/ActionIconWithText.tsx
@@ -1,5 +1,5 @@
 import {ActionIcon} from '@mantine/core'
-import {CSSProperties, ReactNode} from 'react'
+import {CSSProperties, forwardRef, ReactNode} from 'react'
 
 export type ActionIconWithTextProps = {
   text: string
@@ -9,38 +9,34 @@ export type ActionIconWithTextProps = {
   disabled?: boolean
   style?: CSSProperties
 }
-export const ActionIconWithText = ({
-  children,
-  title,
-  text,
-  disabled,
-  style,
-  onClick,
-}: ActionIconWithTextProps) => (
-  <ActionIcon
-    variant='default'
-    size='xl'
-    title={title}
-    disabled={disabled}
-    style={style}
-    onClick={onClick}
-    styles={{
-      icon: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    }}
-  >
-    {children}
-    <div
-      style={{
-        fontSize: '0.75rem',
-        color: 'var(--mantine-color-dimmed)',
+export const ActionIconWithText = forwardRef<HTMLButtonElement, ActionIconWithTextProps>(
+  ({children, title, text, disabled, style, onClick}, ref) => (
+    <ActionIcon
+      ref={ref}
+      variant='default'
+      size='xl'
+      title={title}
+      disabled={disabled}
+      style={style}
+      onClick={onClick}
+      styles={{
+        icon: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
       }}
     >
-      {text}
-    </div>
-  </ActionIcon>
+      {children}
+      <div
+        style={{
+          fontSize: '0.75rem',
+          color: 'var(--mantine-color-dimmed)',
+        }}
+      >
+        {text}
+      </div>
+    </ActionIcon>
+  )
 )
