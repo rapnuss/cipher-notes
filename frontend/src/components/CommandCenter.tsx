6d32e1beb4b0d506b6dabf57e99719ed7b4a8e31
Raphael Nussbaumer <rap.n@icloud.com>
2025-05-14 07:45:53 +0200

    relay on react compiler for memoization

--- frontend/src/components/CommandCenter.tsx
+++ frontend/src/components/CommandCenter.tsx
@@ -11,11 +11,11 @@ import {
 } from '../state/user'
 import {selectAnyDialogOpen, useSelector} from '../state/store'
 import {useMantineColorScheme} from '@mantine/core'
-import {useHotkeys} from '@mantine/hooks'
+import {HotkeyItem, useHotkeys} from '@mantine/hooks'
 import {openSettingsDialog} from '../state/settings'
 import {db} from '../db'
 import {useLiveQuery} from 'dexie-react-hooks'
-import {useCallback, useEffect, useMemo} from 'react'
+import {useEffect} from 'react'
 import {Note} from '../business/models'
 import {exportNotes, openImportDialog, openKeepImportDialog} from '../state/import'
 import {toggleLabelSelector} from '../state/labels'
@@ -28,163 +28,125 @@ export const CommandCenter = () => {
   const anyDialogOpen = useSelector(selectAnyDialogOpen)
   const notes: Note[] = useLiveQuery(() => db.notes.where('deleted_at').equals(0).toArray(), [], [])
 
-  const commands: (SpotlightActionData & {shortcut?: string; onClick: () => void})[] = useMemo(
-    () => [
-      {
-        id: 'toggleColorScheme',
-        label: 'Toggle Dark Mode',
-        onClick: toggleColorScheme,
-        shortcut: 'alt+shift+t',
-      },
-      {
-        id: 'newNote',
-        label: 'New note',
-        onClick: addNote,
-        shortcut: 'alt+shift+n',
-      },
-      {
-        id: 'labelSelector',
-        label: 'Show label selector',
-        onClick: toggleLabelSelector,
-        shortcut: 'alt+shift+l',
-      },
-      {
-        id: 'sync',
-        label: 'Manual server sync',
-        onClick: openSyncDialogAndSync,
-        disabled: !loggedIn,
-      },
-      {
-        id: 'exportNotes',
-        label: 'Export notes',
-        onClick: exportNotes,
-      },
-      {
-        id: 'importNotes',
-        label: 'Import notes',
-        onClick: openImportDialog,
-      },
-      {
-        id: 'keepImportNotes',
-        label: 'Import notes from Keep',
-        onClick: openKeepImportDialog,
-      },
-      {
-        id: 'register',
-        label: 'Register',
-        onClick: openRegisterDialog,
-        disabled: !!email,
-      },
-      {
-        id: 'login',
-        label: 'Login',
-        onClick: openLoginDialog,
-        disabled: loggedIn,
-        shortcut: 'alt+shift+l',
-      },
-      {
-        id: 'encryptionKey',
-        label: 'Encryption-Key (Generate/Import/Export)',
-        onClick: openEncryptionKeyDialog,
-      },
-      {
-        id: 'imprint',
-        label: 'Imprint',
-        onClick: toggleImprint,
-      },
-      {
-        id: 'logout',
-        label: 'Logout',
-        onClick: logout,
-        disabled: !loggedIn,
-        shortcut: 'alt+shift+o',
-      },
-      {
-        id: 'settings',
-        label: 'Settings',
-        onClick: openSettingsDialog,
-        disabled: true,
-      },
-      {
-        id: 'deleteServerNotes',
-        label: 'Delete Server Notes and generate new crypto key',
-        onClick: openDeleteServerNotesDialog,
-        disabled: !loggedIn,
-      },
-      {
-        id: 'changeEmail',
-        label: 'Change Email',
-        onClick: openChangeEmailDialog,
-        disabled: !email,
-      },
-    ],
-    [loggedIn, toggleColorScheme, email]
-  )
+  const commands: (SpotlightActionData & {shortcut?: string; onClick: () => void})[] = [
+    {
+      id: 'toggleColorScheme',
+      label: 'Toggle Dark Mode',
+      onClick: toggleColorScheme,
+      shortcut: 'alt+shift+t',
+    },
+    {
+      id: 'newNote',
+      label: 'New note',
+      onClick: addNote,
+      shortcut: 'alt+shift+n',
+    },
+    {
+      id: 'labelSelector',
+      label: 'Show label selector',
+      onClick: toggleLabelSelector,
+      shortcut: 'alt+shift+l',
+    },
+    {
+      id: 'sync',
+      label: 'Manual server sync',
+      onClick: openSyncDialogAndSync,
+      disabled: !loggedIn,
+    },
+    {
+      id: 'exportNotes',
+      label: 'Export notes',
+      onClick: exportNotes,
+    },
+    {
+      id: 'importNotes',
+      label: 'Import notes',
+      onClick: openImportDialog,
+    },
+    {
+      id: 'keepImportNotes',
+      label: 'Import notes from Keep',
+      onClick: openKeepImportDialog,
+    },
+    {
+      id: 'register',
+      label: 'Register',
+      onClick: openRegisterDialog,
+      disabled: !!email,
+    },
+    {
+      id: 'login',
+      label: 'Login',
+      onClick: openLoginDialog,
+      disabled: loggedIn,
+      shortcut: 'alt+shift+l',
+    },
+    {
+      id: 'encryptionKey',
+      label: 'Encryption-Key (Generate/Import/Export)',
+      onClick: openEncryptionKeyDialog,
+    },
+    {
+      id: 'imprint',
+      label: 'Imprint',
+      onClick: toggleImprint,
+    },
+    {
+      id: 'logout',
+      label: 'Logout',
+      onClick: logout,
+      disabled: !loggedIn,
+      shortcut: 'alt+shift+o',
+    },
+    {
+      id: 'settings',
+      label: 'Settings',
+      onClick: openSettingsDialog,
+      disabled: true,
+    },
+    {
+      id: 'deleteServerNotes',
+      label: 'Delete Server Notes and generate new crypto key',
+      onClick: openDeleteServerNotesDialog,
+      disabled: !loggedIn,
+    },
+    {
+      id: 'changeEmail',
+      label: 'Change Email',
+      onClick: openChangeEmailDialog,
+      disabled: !email,
+    },
+  ]
 
-  const enabledCommands = useMemo(() => commands.filter((c) => !c.disabled), [commands])
+  const enabledCommands = commands.filter((c) => !c.disabled)
 
-  const hotkeys: [string, () => void][] = useMemo(
-    () =>
-      enabledCommands
-        .filter(
-          (c): c is typeof c & {shortcut: string; onClick: () => void} =>
-            !!c.shortcut && !!c.onClick
-        )
-        .map((c) => [c.shortcut, () => !anyDialogOpen && c.onClick()]),
-    [enabledCommands, anyDialogOpen]
-  )
+  const hotkeys: HotkeyItem[] = enabledCommands
+    .filter(
+      (c): c is typeof c & {shortcut: string; onClick: () => void} => !!c.shortcut && !!c.onClick
+    )
+    .map((c) => [c.shortcut, () => !anyDialogOpen && c.onClick()] as const)
 
   useHotkeys(hotkeys, [], true)
 
-  const actions = useMemo(
-    () =>
-      enabledCommands.map(({shortcut, onClick, ...a}) => ({
-        ...a,
-        rightSection: shortcut,
-        onClick: async () => {
-          spotlight.close()
-          await delay(100)
-          onClick()
-        },
-      })),
-    [enabledCommands]
-  )
+  const actions = enabledCommands.map(({shortcut, onClick, ...a}) => ({
+    ...a,
+    rightSection: shortcut,
+    onClick: async () => {
+      spotlight.close()
+      await delay(100)
+      onClick()
+    },
+  }))
 
-  const noteActions = useMemo(
-    () =>
-      notes.map((n) => ({
-        id: n.id,
-        label: n.title,
-        onClick: () => noteOpened(n.id),
-      })),
-    [notes]
-  )
-
-  const actionGroups = useMemo(
-    () => [
-      {
-        group: 'Actions',
-        actions,
-      },
-      {
-        group: 'Notes',
-        actions: noteActions,
-      },
-    ],
-    [actions, noteActions]
-  )
+  const noteActions = notes.map((n) => ({
+    id: n.id,
+    label: n.title,
+    onClick: () => noteOpened(n.id),
+  }))
 
   useEffect(() => {
     window.addEventListener('popstate', spotlight.close)
     return () => window.removeEventListener('popstate', spotlight.close)
-  }, [])
-
-  const onOpen = useCallback(() => {
-    history.pushState(null, '', location.href)
-  }, [])
-
-  const onClose = useCallback(() => {
-    history.back()
   }, [])
 
   return (
@@ -196,9 +158,18 @@ export const CommandCenter = () => {
       maxHeight='100%'
       disabled={anyDialogOpen}
       limit={actions.length}
-      actions={actionGroups}
-      onSpotlightOpen={onOpen}
-      onSpotlightClose={onClose}
+      actions={[
+        {
+          group: 'Actions',
+          actions,
+        },
+        {
+          group: 'Notes',
+          actions: noteActions,
+        },
+      ]}
+      onSpotlightOpen={() => history.pushState(null, '', location.href)}
+      onSpotlightClose={() => history.back()}
     />
   )
 }
